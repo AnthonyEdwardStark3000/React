ff9e80a90e5dfe414bb87cef1b8421038a7ea2ce
AnthonyEdwardStark3000 <catchmesureshbabu8@gmail.com>
2022-03-15 00:04:41 +0530

    Refreshed the basix and now having some sort of worry

---
+++ simple-registeration-form/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const image = "https://picsum.photos/200";
+  return (
+    <div className="App">
+      <header className="App-header">
+      {/*<img src = {image + "?grayscale"}/> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
