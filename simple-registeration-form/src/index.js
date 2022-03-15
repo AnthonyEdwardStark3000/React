c4fb9f5b6bca50f2f9d7571d400e6a15d1107662
AnthonyEdwardStark3000 <catchmesureshbabu8@gmail.com>
2022-03-15 18:22:00 +0530

    feeling ill but back to learn the basics. Happy coding !

--- simple-registeration-form/src/index.js
+++ simple-registeration-form/src/index.js
@@ -1,47 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const name = "Suresh";
-const age = 22;
-const currentDate = new Date();
-const currentyear = currentDate.getFullYear();  
-// const url ="https://media.gettyimages.com/photos/robert-downey-jr-attends-the-premiere-of-columbia-pictures-spiderman-picture-id803015182?k=20&m=803015182&s=612x612&w=0&h=f4UIm0rkrdUvrnRI1I0LQPIIjiRxucAewdfgwQVC-H8=";
-const url = "https://picsum.photos/200";
-ReactDOM.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
-  <>
-  <p contentEditable = "true" spellCheck="false"> Created By {`${name}`}'s'
-  Kathupom 
-  loremsdfbhsdjfsbhdfhsdbfsldfhbsdlfblfshdfhsdlfsdlfsdfhbsdfldfhbldhfbdslfbsdfhldbsflsdhbfsdl
-  loremsdfbhsdjfsbhdfhsdbfsldfhbsdlfblfshdfhsdlfsdlfsdfhbsdfldfhbldhfbdslfbsdfhldbsflsdhbfsdl
-  loremsdfbhsdjfsbhdfhsdbfsldfhbsdlfblfshdfhsdlfsdlfsdfhbsdfldfhbldhfbdslfbsdfhldbsflsdhbfsdl
-  loremsdfbhsdjfsbhdfhsdbfsldfhbsdlfblfshdfhsdlfsdlfsdfhbsdfldfhbldhfbdslfbsdfhldbsflsdhbfsdl
-  loremsdfbhsdjfsbhdfhsdbfsldfhbsdlfblfshdfhsdlfsdlfsdfhbsdfldfhbldhfbdslfbsdfhldbsflsdhbfsdl
-  </p>
-  <p> Created By {name}'s'
-  loremsdfbhsdjfsbhdfhsdbfsldfhbsdlfblfshdfhsdlfsdlfsdfhbsdfldfhbldhfbdslfbsdfhldbsflsdhbfsdl
-  loremsdfbhsdjfsbhdfhsdbfsldfhbsdlfblfshdfhsdlfsdlfsdfhbsdfldfhbldhfbdslfbsdfhldbsflsdhbfsdl
-  loremsdfbhsdjfsbhdfhsdbfsldfhbsdlfblfshdfhsdlfsdlfsdfhbsdfldfhbldhfbdslfbsdfhldbsflsdhbfsdl
-  loremsdfbhsdjfsbhdfhsdbfsldfhbsdlfblfshdfhsdlfsdlfsdfhbsdfldfhbldhfbdslfbsdfhldbsflsdhbfsdl
-  </p>
-  <ul>
-  <li>apple</li>
-  <li>orange</li>
-  <li>banana</li>
-  </ul>
-  <img className="ImageDisplay" src={url+"?grayscale"} ></img>
-  <h2 className="heading">And he is just <span> {age-1} </span>years old</h2>
-  <h3>{`Copyright@${currentyear}`}</h3>
-  </>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import React from "react";
+import ReactDom from "react-dom";
+const timeNow = new Date().getHours();
+let message;
+const name ="Mr.Stark";
+const myStyle={
+    fontSize: "2rem",
+    fontWeight: "bold",
+    fontStyle: "italic",
+    borderBottom: "5px solid black",
+    color:""
+}
+if(timeNow <12)
+{
+    message = "Good morning";
+    myStyle.color ="red"
+}
+else if (timeNow < 18)
+{
+    message = "Good afternoon";
+    myStyle.color ="blue"
+    
+}
+else
+{
+    message = "Good evening";
+    myStyle.color ="green"
+}
+ReactDom.render(<h1 style={myStyle}>{message} {name}</h1>,document.getElementById('root'));
