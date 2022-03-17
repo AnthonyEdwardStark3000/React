1fd696e30bcd5a0a266f4f061e408ef33e83d875
AnthonyEdwardStark3000 <catchmesureshbabu8@gmail.com>
2022-03-17 23:27:50 +0530

    Hope me undestand all the logics stuff and Goods well in React, till then will continue my work

---
+++ keeper/src/component/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(){
+    return(
+        <div className="note">
+            <h1>Hi you</h1>
+            <p>I would like to say something to you . You are giving an awesome try, Just plz continue it until other's notice you</p>
+        </div>
+    )
+}
+
+export default Note
