6bad6681ab69f6151350a8d9d1d1de5e24a660bd
AnthonyEdwardStark3000 <catchmesureshbabu8@gmail.com>
2022-03-26 20:14:37 +0530

    Destructuring practice and its quite confusing

---
+++ destructuring/src/App.js
@@ -0,0 +1,22 @@
+import animals from "./Data";
+import useAnimals from "./Data";
+import './App.css';
+
+// console.log(animals[1].name);
+const [cat, dog]=animals;
+// console.log(cat.sound)
+// const {name,sound,feedingRequirements} = cat;
+// console.log(feedingRequirements);
+
+const [animal, makeSound] = useAnimals(cat);
+console.log(animal);
+makeSound();
+
+function App() {
+  return (
+    // <h1>{sound}</h1>
+    <></>
+  );
+}
+
+export default App;
