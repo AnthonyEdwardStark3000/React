791cbdf2adfb49dc3ff94d67cd154f05f20a68f8
AnthonyEdwardStark3000 <catchmesureshbabu8@gmail.com>
2022-03-27 16:40:13 +0530

    The errors in React js is simply unbelieveably weird

--- logineventhandling/src/App.js
+++ logineventhandling/src/App.js
@@ -3,8 +3,14 @@ import React, { useState } from "react";
 function App() {
   const [welcome, setHeadingText] = useState("Hello");
   const [isMousedOver, setMouseOver] = useState(false);
-  function handleClick(){
-    setHeadingText("Submitted")
+  const [name, setName] = useState("");
+  const [headingText, setHeading] = useState("");
+
+  function handleClick(event){
+    setHeadingText("Submitted");
+    // setName(event.target.value);
+    setHeading(name);
+    // event.preventDefault();
   }
 
   function hover(){
@@ -16,15 +22,22 @@ function App() {
     setMouseOver(false);
   }
 
+  function handleChange(event){
+    console.log(event.target.value);
+    setName(event.target.value);
+  }
+
   return (
     <div className="container">
-      <h1>{welcome}</h1>
-      <input type="text" placeholder='Whats your name?'></input>
-      <button style={{ backgroundColor: isMousedOver ? "black":"white" }} 
-       onClick={handleClick}
+      <h1>{welcome} {headingText}</h1>
+      {/* <form> *form tags usually refresh to make post refresh */}
+      <input onChange={handleChange} type="text" placeholder='Whats your name?' value={name}></input>
+      <button type="submit" style={{ backgroundColor: isMousedOver ? "black":"white" }} 
+       onClick={()=>handleClick()} //lambda to ignore re render error
        onMouseOver={hover}
        onMouseOut={hoverOver}
        >Submit</button>
+      {/* </form> */}
     </div>
   );
 }
