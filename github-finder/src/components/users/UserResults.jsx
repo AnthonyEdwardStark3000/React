fc9a582a42a321cbf8d2ab95d278216f6ed85651
AnthonyEdwardStark3000 <catchmesureshbabu8@gmail.com>
2022-04-10 21:24:36 +0530

    Checking with the API issue

--- github-finder/src/components/users/UserResults.jsx
+++ github-finder/src/components/users/UserResults.jsx
@@ -2,30 +2,44 @@ import {useContext} from 'react';
 import Spinner from '../Layouts/Spinner';
 import UserItem from './UserItem';
 import GithubContext from '../context/github/GithubContext';
+import { useEffect, useState } from 'react';
 
 function UserResults() {
-    const {users, loading} = useContext(GithubContext);
+    const[users, setUsers] = useState([]);
+    const[loading, setLoading] = useState([]);
 
-    // useEffect(function(){
-    //     fetchUsers()    
-    // },[])
+    // const {users, loading} = useContext(GithubContext);
 
-    if(!loading){
-        return (
-          <div className="grid grid-cols-1 gap-8 xl: grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-          { 
-           users.map((user)=>(
-              <UserItem key={user.id} user={user} />    
-           ))
-            }
-          </div>
-        )
+    useEffect(function(){
+        fetchUsers()    
+    },[]);
+
+    const fetchUsers = async()=>{
+        const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users/AnthonyEdwardStark3000`,{
+            headers: {
+                Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+            },
+        })
+        const data = await response.json();
+        console.log(data)
     }
-    else{
-        return(
-            <Spinner />
-        )
-    }
+
+    // if(!loading){
+    //     return (
+    //       <div className="grid grid-cols-1 gap-8 xl: grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+    //       { 
+    //        users.map((user)=>(
+    //           <UserItem key={user.id} user={user} />    
+    //        ))
+    //         }
+    //       </div>
+    //     )
+    // }
+    // else{
+    //     return(
+    //         <Spinner />
+    //     )
+    // }
 }
 
 export default UserResults
